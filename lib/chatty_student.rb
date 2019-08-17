443038aefdedd32926810cb48854d71ef2d36ea5
Randy Taylor <randyisbusy@gmail.com>
2019-08-17 21:57:09 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,8 @@
-class ChattyStudent < Student 
-  def hello 
+class ChattyStudent < Student
+  def hello
     super
       puts "How are you doing Today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
-  end 
+  end
   def raise_hand
     super
       10.times puts "Pick me!"
