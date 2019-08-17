d3a53b60110dab9fab3ff5483250a8c47b46463a
Randy Taylor <randyisbusy@gmail.com>
2019-08-17 22:08:26 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,7 +4,7 @@ class ChattyStudent < Student
        puts "How are you doing Today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-    super
-    10.times  puts "Pick me!"
+    super 10.times
+   "Pick me!"
     end
 end
